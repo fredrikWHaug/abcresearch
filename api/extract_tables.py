d769d9d7b3fd5c53c8aba9fa029670b621b7e4cd
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2025-09-28 22:54:40 -0400

    edge function bypass for deployment to work, attempt3 - cors header issue.

--- api/extract_tables.py
+++ api/extract_tables.py
@@ -12,9 +12,10 @@ def handler(request):
     """
     # Set CORS headers
     headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'POST, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Origin': 'https://www.developent.guru',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS, GET',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Access-Control-Max-Age': '86400',
         'Content-Type': 'application/json'
     }
 
