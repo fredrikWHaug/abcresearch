8e5a00aa4d9421a4da9118fc77f265a7cf3b75a9
Sofie Yang <syang@mba2026.hbs.edu>
2025-10-14 17:16:28 -0400

    refactor: update services to use centralized types

--- src/services/paperLinkingService.ts
+++ src/services/paperLinkingService.ts
@@ -3,7 +3,7 @@
 
 import { pubmedAPI } from './pubmedAPI';
 import type { PubMedArticle } from './pubmedAPI';
-import type { ClinicalTrial } from './clinicalTrialsAPI';
+import type { ClinicalTrial } from '@/types/trials';
 
 export interface PaperTrialLink {
   trial: ClinicalTrial;
